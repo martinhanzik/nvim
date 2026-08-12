59febdb90528a269c427ceb6e5bdd44418d4487c
Martin Hanzík <martin@hanzik.com>
2026-08-12 10:16:17 +0200

    fix clipboard

--- init.lua
+++ init.lua
@@ -119,10 +119,22 @@ do
   vim.o.showmode = false
 
   -- Sync clipboard between OS and Neovim.
-  --  Schedule the setting after `UiEnter` because it can increase startup-time.
-  --  Remove this option if you want your OS clipboard to remain independent.
-  --  See `:help 'clipboard'`
-  vim.schedule(function() vim.o.clipboard = 'unnamedplus' end)
+  if vim.env.ZELLIJ or vim.env.SSH_TTY then
+    -- Zellij eats nvim's terminal capability query, so force the OSC 52 provider.
+    local osc52 = require 'vim.ui.clipboard.osc52'
+    local function paste()
+      return vim.split(vim.fn.getreg '"', '\n')
+    end
+    vim.g.clipboard = {
+      name = 'OSC 52',
+      copy = { ['+'] = osc52.copy '+', ['*'] = osc52.copy '*' },
+      paste = { ['+'] = paste, ['*'] = paste },
+    }
+  else
+    vim.schedule(function()
+      vim.o.clipboard = 'unnamedplus'
+    end)
+  end
 
   -- Enable break indent
   vim.o.breakindent = true
@@ -248,7 +260,12 @@ do
   vim.api.nvim_create_autocmd('TextYankPost', {
     desc = 'Highlight when yanking (copying) text',
     group = vim.api.nvim_create_augroup('kickstart-highlight-yank', { clear = true }),
-    callback = function() vim.hl.on_yank() end,
+    callback = function()
+      vim.hl.on_yank()
+      if vim.g.clipboard and vim.g.clipboard.name == 'OSC 52' and vim.v.event.operator == 'y' then
+        vim.g.clipboard.copy['+'](vim.v.event.regcontents)
+      end
+    end,
   })
 end
 
